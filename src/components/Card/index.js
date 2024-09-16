7477d2f9f58b11677151d241d9119b8f43ab67dc
Peangelo <peangelorafa@gmail.com>
2024-09-16 16:34:12 -0300

    adicionando cards

---
+++ src/components/Card/index.js
@@ -0,0 +1,21 @@
+import styles from './Card.module.css'
+import iconeFavoritar from './favoritar.png'
+
+function Card ({id, capa, titulo}) {
+    return(
+        <div className={styles.container}>
+            <img 
+                src={capa} 
+                alt={titulo} 
+                className={styles.capa}/>
+            <h2>{titulo}</h2>
+            <img 
+                src={iconeFavoritar} 
+                alt='Favoritar filmes' 
+                className={styles.favoritar}/>
+        </div>
+    )
+
+}
+
+export default Card
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,6 +2,7 @@ import Rodape from "../../components/Rodape";
 import Cabecalho from "../../components/Cabecalho";
 import Banner from "../../components/Banner";
 import Titulo from "../../components/Titulo";
+import Card from "../../components/Card";
 
 function Inicio () {
 
@@ -12,6 +13,7 @@ function Inicio () {
             <Titulo>
                 <h1>Um lugar para guardar seus videos e filmes</h1>
             </Titulo>
+            <Card id='1' titulo='teste' capa='https://github.com/Peangelo.png'/>
             <Rodape/>
         </>
     )
