d3fa755ac9ed4d36a84cb35eee7deced7eefe9bb
Peangelo <peangelorafa@gmail.com>
2024-09-17 09:40:35 -0300

    arrumando cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "../../components/Cabecalho";
 import Banner from "../../components/Banner";
 import Titulo from "../../components/Titulo";
 import Card from "../../components/Card";
+import videos from '../../json/db.json'
+import styles from './Inicio.module.css'
 
 function Inicio () {
 
@@ -13,7 +15,11 @@ function Inicio () {
             <Titulo>
                 <h1>Um lugar para guardar seus videos e filmes</h1>
             </Titulo>
-            <Card id='1' titulo='teste' capa='https://github.com/Peangelo.png'/>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key ={video.id} />
+                })}
+            </section>
             <Rodape/>
         </>
     )
